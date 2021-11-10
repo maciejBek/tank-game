547a13e45567518305be695897390b56af794278
maciejBek <maciek8928@o2.pl>
2021-11-10 22:25:53 +0100

    initial commit

---
+++ backend/src/main/java/pl/company/tankgame/TankGameApplication.java
@@ -0,0 +1,13 @@
+package pl.company.tankgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankGameApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/company/tankgame/TankGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.company.tankgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
