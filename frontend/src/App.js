b5e1d6b103a14456541123bf491fa46e04fb41d6
Maciej <maciek8928@o2.pl>
2022-01-29 22:40:46 +0100

    send the coordinates back to the players in the relevant room

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
+
+var connected = false;
+var socket ='';
+var stompClient = '';
+var roomId = -1;
+
+const connect = () => {
+  socket = new SockJS("http://localhost:8080/stomp");
+  stompClient = Stomp.over(socket);
+  stompClient.connect(
+    {},
+    frame => {
+      connected = true;
+      roomId = document.getElementById("room-id").value;
+      stompClient.subscribe("/topic/coordinates/" + roomId, tick => {
+      });
+    },
+    error => {
+      console.log(error);
+      connected = false;
+      }
+  );
+}
+
+const send = () => {
+  let playerId = 1; // pobrac id od biezacego gracza
+  let xCoordinate = document.getElementById("x-coordinate").value;
+  let yCoordinate = document.getElementById("y-coordinate").value;
+
+  if (stompClient && stompClient.connected) {
+    const data = { 
+      x: xCoordinate, 
+      y: yCoordinate,
+      playerId: playerId};
+    stompClient.send("/app/hello/" + roomId, {}, JSON.stringify(data));
+  }
+}
+
+const disconnect = () => {
+  if (stompClient) {
+    stompClient.disconnect();
+  }
+    connected = false;
+}
+
+const tickleConnection = () => {
+  connected ? disconnect() : connect();
+} 
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <input type="text" id="room-id" />
+        <button onClick={connect}>Connect</button>
+        <input type="text" id="x-coordinate" />
+        <input type="text" id="y-coordinate" />
+        <button onClick={send}>Send</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,3 +81,4 @@ function App() {
 }
 
 export default App;
+
