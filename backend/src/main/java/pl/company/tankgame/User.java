9696b9263d3b90f1ad4571e3a50e8cdebde197fd
Maciej <maciek8928@o2.pl>
2022-02-05 21:28:32 +0100

    add class

---
+++ backend/src/main/java/pl/company/tankgame/User.java
@@ -0,0 +1,24 @@
+package pl.company.tankgame;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
