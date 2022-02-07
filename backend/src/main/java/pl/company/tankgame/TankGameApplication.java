9809b16635549394f1c668b05784859b385a3de8
Maciej <maciek8928@o2.pl>
2022-02-07 12:07:00 +0100

    minor changes

--- backend/src/main/java/pl/company/tankgame/TankGameApplication.java
+++ backend/src/main/java/pl/company/tankgame/TankGameApplication.java
@@ -2,11 +2,10 @@ package pl.company.tankgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+//@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class TankGameApplication {
 
     private static ApplicationContext applicationContext;
