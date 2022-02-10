fc5bdb87f774ae8b3d880f207958258ab394b6cc
Filip50n <nic990000@gmail.com>
2022-02-10 22:00:58 +0100

    gra i interfejs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 import Game from './components/state/Game'
+import Logowanie from './components/state/Logowanie'
+import Rejestracja from './components/state/Rejestracja'
 import './App.css'
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
 
 class App extends Component {
 
     render() {
         return (
+        <Router>
+            <Routes>
+            <Route exact path="/" element={<Logowanie/>}></Route>
+            <Route exact path="/Rejestracja" element={<Rejestracja/>}></Route>
+            <Route exact path="/game" element={<Game/>}></Route>
+                
 
-                <Game  />
+
+                
+            
+            </Routes>
+        </Router>  
 
         );
     }
